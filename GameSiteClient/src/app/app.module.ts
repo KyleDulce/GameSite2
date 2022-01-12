a2b5f038a8bcc6bac1058ea4d7d9d946b9247f41
Kevin Le <kevinle@Kevins-MacBook-Pro.local>
2022-01-11 22:32:32 -0500

    Added 2 Components

--- GameSiteClient/src/app/app.module.ts
+++ GameSiteClient/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './components/home/home-page/home-page.component';
+import { SupportPageComponent } from './components/supportpage/support-page/support-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SupportPageComponent
   ],
   imports: [
     BrowserModule,
