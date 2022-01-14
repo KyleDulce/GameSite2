067cf9a8bdebb8d1db24a434e9ecfcadebcde4b9
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2022-01-13 20:07:33 -0500

    Added new docs

--- GameSiteClient/src/app/app-routing.module.ts
+++ GameSiteClient/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home/home-page/home-page.component';
+import { SupportPageComponent } from './components/supportpage/support-page/support-page.component';
 
 
 
 const routes: Routes = [
 
   {path:"", component: HomePageComponent},
+  {path:"support", component: SupportPageComponent},
   
 
 ];
