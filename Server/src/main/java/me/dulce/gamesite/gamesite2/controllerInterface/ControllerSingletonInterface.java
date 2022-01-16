c20018784441a9e30a1381a43e00dbd47b6adda0
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2022-01-16 00:12:05 -0500

    Added static resource mapping

--- Server/src/main/java/me/dulce/gamesite/gamesite2/controllerInterface/ControllerSingletonInterface.java
+++
@@ -1,5 +0,0 @@
-package me.dulce.gamesite.gamesite2.controllerInterface;
-
-public interface ControllerSingletonInterface {
-    protected void setupSingleton();
-}
--- Server/src/main/java/me/dulce/gamesite/gamesite2/transportcontroller/RestWebController.java
+++ Server/src/main/java/me/dulce/gamesite/gamesite2/transportcontroller/RestWebController.java
@@ -1,11 +1,18 @@
 package me.dulce.gamesite.gamesite2.transportcontroller;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @Controller
@@ -28,4 +35,13 @@ public class RestWebController {
     public RedirectView index() {
         return new RedirectView("/" + FRONTEND_ENDPOINT_GROUP);
     }
+
+	//any page in /pages returns static resource
+	@RequestMapping("/pages/**")
+	public ModelAndView requestAnyPath() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index.html");
+		return mav;
+	}
+	
 }
