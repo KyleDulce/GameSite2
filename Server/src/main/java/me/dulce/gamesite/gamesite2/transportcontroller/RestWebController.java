7ee9bda9d4964e26b83f682caf18b68f8a3f9659
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2022-01-16 01:00:44 -0500

    Added feature to serve static resources

--- Server/src/main/java/me/dulce/gamesite/gamesite2/transportcontroller/RestWebController.java
+++ Server/src/main/java/me/dulce/gamesite/gamesite2/transportcontroller/RestWebController.java
@@ -1,21 +1,15 @@
 package me.dulce.gamesite.gamesite2.transportcontroller;
 
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
-
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@Controller
 public class RestWebController {
 
     public static final String FRONTEND_ENDPOINT_GROUP = "pages";
@@ -31,17 +25,30 @@ public class RestWebController {
 		};
 	}
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public RedirectView index() {
         return new RedirectView("/" + FRONTEND_ENDPOINT_GROUP);
     }
 
 	//any page in /pages returns static resource
-	@RequestMapping("/pages/**")
+	@RequestMapping("/pages")
 	public ModelAndView requestAnyPath() {
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("index.html");
+		mav.setViewName("/index.html");
 		return mav;
+	}
+
+	@RequestMapping("/pages/{requested}")
+	public ModelAndView requestAnyPathChild(@PathVariable("requested") String requested) {
+		if(requested != "" && requested.matches(".+\\..+")) {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName(requested);
+			return mav;
+		} else {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("/pages");
+			return mav;	
+		}
 	}
 	
 }
